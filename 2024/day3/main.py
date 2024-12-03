eecd6451a32e99d420a96fa7ba5c7c3f060e7b54
rice-cracker-dev <ricecracker2234@outlook.com>
2024-12-03 21:34:10 +0700

    rewrote day 3 with regex

--- 2024/day3/main.py
+++ 2024/day3/main.py
@@ -1,84 +1,49 @@
+from re import search
+
+
+mulRegex = "^mul\\([0-9]*,[0-9]*\\)"
+doRegex = "^do\\(\\)"
+dontRegex = "^don't\\(\\)"
+
+
 def readInputs() -> str:
-    with open("./inputs.txt", "r") as f:
-        return f.read()
+    with open("./inputs.txt", "r") as f: return f.read()
 
 
-# return number, length
-def scanForNumber(s: str, index: int, builder: str = "") -> tuple[int | None, int]:
-    if index >= len(s): return None, 0
-
-    if not s[index].isnumeric():
-        length = len(builder)
-        if length > 0: return int(builder), length
-        
-        return None, 0
-
-    return scanForNumber(s, index + 1, builder + s[index])
-
-
-# parsing madness
-def scanForMul(s: str, i: int) -> tuple[int | None, int | None]:
-    if len(s) - i < len("mul("): return None, None
-
-    keyword = s[i:i + len("mul")]
-    functionStart = s[i + len("mul")]
-
-    if keyword != "mul": return None, None
-    if functionStart != "(": return None, None
-
-    aIndex = i + len("mul") + 1
-    a, aL = scanForNumber(s, aIndex)
-
-    if not a: return None, None
-    if s[aIndex + aL] != ",": return None, None
-
-    bIndex = aIndex + aL + 1
-    b, bL = scanForNumber(s, bIndex)
-
-    if not b: return None, None
-    if s[bIndex + bL] != ")": return None, None
-
-    return a, b
-
-
-def scanForDo(s: str, i: int) -> bool:
-    keyword = s[i:i + len("do()")]
-    return keyword == "do()"
-
-
-def scanForDont(s: str, i: int) -> bool:
-    keyword = s[i:i + len("don't()")]
-    return keyword == "don't()"
+def parseMul(s: str) -> int:
+    numbers = [int(x) for x in s[len("mul("):-len(")")].split(',')]
+    return numbers[0] * numbers[1]
 
 
 def main():
     corrupted = readInputs()
-    corruptedLength = len(corrupted)
-    scannedSum = 0
-    scannedDoSum = 0
+    total = 0
+    totalDo = 0
 
-    # i should've learnt regex
     do = True
-    for i in range(corruptedLength):
-        if corruptedLength - i >= len("do()") and scanForDo(corrupted, i):
+    for i in range(len(corrupted)):
+        s = corrupted[i:]
+
+        if search(doRegex, s):
             do = True
             continue
 
-        if corruptedLength - 1 >= len("don't()") and scanForDont(corrupted, i):
+        if search(dontRegex, s):
             do = False
             continue
 
-        a, b = scanForMul(corrupted, i);
+        matches = search(mulRegex, s)
 
-        if a and b:
-            mul = a * b
-            scannedSum += mul
-            if do: scannedDoSum += mul
-            
-            print(f"found: {a} * {b}")
+        if not matches: continue
 
-    print(f"part1: {scannedSum}")
-    print(f"part2: {scannedDoSum}")
+        parsed = parseMul(matches.group(0))
+        total += parsed
+        if do: totalDo += parsed
+        
+    print(f"part1: {total}")
+    print(f"part2: {totalDo}")
+
 
 if __name__ == "__main__":
     main()
+
